ecbd1f851680aea8f21d5c4431a69e8a43d8f91a
Deepa Suba <deepasuba313@gmail.com>
2025-11-29 17:50:50 +0530

    29-11-2025

---
+++ Divisionbyzero.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.Scanner.*;
+
+public class Divisionbyzero
+{
+public static void main (String args[]) 
+	{
+	int a=100;
+	int b;
+	System.out.println("Enter the input value: ");
+	Scanner ip=new Scanner(System.in);
+	b=ip.nextInt();
+	try 
+		{
+		 int result = 100 / b;
+		 System.out.println("-----Divisible by Zero Error-----");
+		 System.out.println("The final result after printing was : "+result);
+		}
+	catch(Exception e) 
+		{
+		System.out.println(e.getMessage());
+		System.out.println("Error: Division by zero is not allowed.");
+		}
+	finally 
+		{
+		System.out.println("Program finished.");
+		}
+	}
+}
